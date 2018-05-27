23e67bf7d268b3847b78780ecc2f47995f1fea11
ahmad-mardene <[ahmad.yoness@gmail.com]>
2018-05-27 17:01:10 +0200

    new

--- script.js
+++ script.js
@@ -8,126 +8,84 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var location = /** @class */ (function () {
-    function location(Name, ZipCode, Address, Image) {
+var locaion = /** @class */ (function () {
+    function locaion(Name, ZipCode, Adress, Image, Telephone) {
         this.Name = Name;
         this.ZipCode = ZipCode;
-        this.Address = Address;
+        this.Adress = Adress;
         this.Image = Image;
+        this.Telephone = Telephone;
     }
-    ;
-    return location;
+    return locaion;
 }());
-;
-var Place = /** @class */ (function (_super) {
-    __extends(Place, _super);
-    function Place(Name, ZipCode, Address, Image, sort) {
-        var _this = this;
-        _this.sort = "Place";
+var place = /** @class */ (function (_super) {
+    __extends(place, _super);
+    function place(Name, ZipCode, Adress, Image, type) {
+        var _this = _super.call(this, Name, ZipCode, Adress, Image) || this;
+        _this.type = "place";
         return _this;
     }
-    ;
-    return Place;
-}(location));
-;
-var Restaurant = /** @class */ (function (_super) {
-    __extends(Restaurant, _super);
-    function Restaurant(Name, ZipCode, Address, Image, Telephone, Type, Website, sort) {
-        var _this = _super.call(this, Name, ZipCode, Address, Image) || this;
-        _this.sort = "Restaurant";
-        _this.Telephone = "Telephone";
-        _this.Type = "Type";
-        _this.Website = "Website";
+    return place;
+}(locaion));
+var events = /** @class */ (function (_super) {
+    __extends(events, _super);
+    function events(Name, ZipCode, Adress, Image, type) {
+        var _this = _super.call(this, Name, ZipCode, Adress, Image) || this;
+        _this.type = "events";
         return _this;
     }
-    ;
-    return Restaurant;
-}(location));
-;
-var Event = /** @class */ (function (_super) {
-    __extends(Event, _super);
-    function Event(Name, ZipCode, Address, Image, Date, Time, TeckitPrice, sort) {
-        var _this = _super.call(this, Name, ZipCode, Address, Image) || this;
-        _this.sort = "Event";
-        _this.Date = "Date";
-        _this.Time = "Time";
-        _this.TeckitPrice = "Teckit Price";
+    return events;
+}(locaion));
+var reataurant = /** @class */ (function (_super) {
+    __extends(reataurant, _super);
+    function reataurant(Name, ZipCode, Adress, Image, type) {
+        var _this = _super.call(this, Name, ZipCode, Adress, Image) || this;
+        _this.type = "reataurant";
         return _this;
     }
-    ;
-    return Event;
-}(location));
-;
-var place1 = new Place("St. Charles Church", "Vienna 1010", "Karlsplatz 1", "src = img/Karlskirche.jpg");
-var place2 = new Place("Vienna Zoo", "Vienna 1130", "Maxingstraße 13b", "src = img/viennazoo.jpg");
-var restaurant1 = new Restaurant("Lemon Leaf", "Vienna 1050", "Kettenbrückengasse 19", "image", "+43(1)5812308", "Thai Restaurant", "www.lemonleaf.at");
-var restaurant2 = new Restaurant("SIXTA", "Vienna 1050", "Schönbrunner Straße 21", "image", "+43 1 58 528 56 | +43 1 58 528 56", "wienerisch", "www.sixta-restaurant.at");
-var event1 = new Event("Kris Kristofferson", "Vienna 1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "image", "Fr., 15. Jun.", "20:00", "58,50 EUR");
-var event2 = new Event("Lenny Kravitz", "Vienna1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "image", "Sat, 09.06.2018", "19:30", "€ 47,80");
-var data = [];
-data.push(place1, place2, restaurant1, restaurant2, event1, event2);
+    return reataurant;
+}(locaion));
+var place1 = new place("St. Charles Church", "Vienna 1010", "Karlsplatz 1", "Image");
+var place2 = new place("Vienna Zoo", "Vienna 1130", "Maxingstraße 13b", "Image");
+var reataurant1 = new reataurant("Lemon Leaf", "Vienna 1050", "Kettenbrückengasse 19", "image", "+43(1)5812308", "Thai Restaurant", "www.lemonleaf.at");
+var reataurant2 = new reataurant("SIXTA", "Vienna 1050", "Schönbrunner Straße 21", "image", "+43 1 58 528 56 | +43 1 58 528 56", "wienerisch", "www.sixta-restaurant.at");
+var events1 = new events("Kris Kristofferson", "Vienna 1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1", "image", "Fr., 15. Jun.", "20:00", "58,50 EUR");
+var events2 = new events("Lenny Kravitz", "Vienna1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "image", "Sat, 09.06.2018", "19:30", "€ 47,80");
+var locaion_data = [];
+locaion_data.push(place1, place2, reataurant1, reataurant2, events1, events2);
 function createDOM() {
-    for (i = 0; i < data.length; i++) {
+    for (i = 0; i < locaion_data.length; i++) {
         dummy = document.createElement("div");
         dummy.classList.add("card");
-        dummy.classList.add("col-lg-3");
+        dummy.classList.add("col-lg-4");
         dummy.classList.add("col-md-6");
         dummy.classList.add("col-sm-12");
         var row = document.getElementById("row4");
         row.appendChild(dummy);
         var headline = document.createElement("h4");
-        var headline_text = document.createTextNode(data[i].Name);
+        var headline_text = document.createTextNode(locaion_data[i].Name);
         headline.appendChild(headline_text);
         dummy.appendChild(headline);
-        var name = document.createElement("p");
-        var name_text = document.createTextNode(data[i].ZipCode);
-        name.appendChild(name_text);
-        dummy.appendChild(name);
-        var address = document.createElement("p");
-        var address_text = document.createTextNode(data[i].Address);
-        address.appendChild(address_text);
-        dummy.appendChild(address);
-        var zipCode = document.createElement("p");
-        var zipCode_text = document.createTextNode(data[i].ZipCode);
-        zipCode.appendChild(zipCode_text);
-        dummy.appendChild(zipCode);
-        var image = document.createElement("p");
-        var image_text = document.createTextNode(data[i].Image);
-        image.appendChild(image_text);
-        dummy.appendChild(image);
-        var telephone = document.createElement("p");
-        var telephone_text = document.createTextNode(data[i].Telephone);
-        telephone.appendChild(telephone_text);
-        dummy.appendChild(telephone);
-        var type = document.createElement("p");
-        var type_text = document.createTextNode(data[i].Type);
-        type.appendChild(type_text);
-        dummy.appendChild(type);
-        var website = document.createElement("p");
-        var website_text = document.createTextNode(data[i].Website);
-        website.appendChild(website_text);
-        dummy.appendChild(website);
-        var date = document.createElement("p");
-        var date_text = document.createTextNode(data[i].Date);
-        date.appendChild(date_text);
-        dummy.appendChild(date);
-        var time = document.createElement("p");
-        var time_text = document.createTextNode(data[i].Time);
-        time.appendChild(time_text);
-        dummy.appendChild(time);
-        var teckitPrice = document.createElement("p");
-        var teckitPrice_text = document.createTextNode(data[i].TeckitPrice);
-        teckitPrice.appendChild(teckitPrice_text);
-        dummy.appendChild(teckitPrice);
+        var Adress = document.createElement("p");
+        var Adress_text = document.createTextNode(locaion_data[i].Adress);
+        Adress.appendChild(Adress_text);
+        dummy.appendChild(Adress);
+        var ZipCode = document.createElement("p");
+        var ZipCode_text = document.createTextNode(locaion_data[i].ZipCode);
+        ZipCode.appendChild(ZipCode_text);
+        dummy.appendChild(ZipCode);
+        var Image = document.createElement("p");
+        var Image_text = document.createTextNode(locaion_data[i].Image);
+        Image.appendChild(Image_text);
+        dummy.appendChild(Image);
     }
-    ;
 }
-;
+// createDOM();
 function createTabs(arr, x) {
     for (i = 0; i < arr.length; i++) {
         dummy = document.createElement("div");
         dummy.classList.add("card");
-        dummy.classList.add("col-lg-3");
+        dummy.classList.add("col-lg-4");
         dummy.classList.add("col-md-6");
         dummy.classList.add("col-sm-12");
         var row = document.getElementById("row" + x);
@@ -136,49 +94,23 @@ function createTabs(arr, x) {
         var headline_text = document.createTextNode(arr[i].Name);
         headline.appendChild(headline_text);
         dummy.appendChild(headline);
-        var address = document.createElement("p");
-        var address_text = document.createTextNode(arr[i].Address);
-        address.appendChild(address_text);
-        dummy.appendChild(address);
-        var zipCode = document.createElement("p");
-        var zipCode_text = document.createTextNode(arr[i].ZipCode);
-        zipCode.appendChild(zipCode_text);
-        dummy.appendChild(zipCode);
-        var image = document.createElement("p");
-        var image_text = document.createTextNode(arr[i].Image);
-        image.appendChild(image_text);
-        dummy.appendChild(image);
-        var telephone = document.createElement("p");
-        var telephone_text = document.createTextNode(arr[i].Telephone);
-        telephone.appendChild(telephone_text);
-        dummy.appendChild(telephone);
-        var type = document.createElement("p");
-        var type_text = document.createTextNode(arr[i].Type);
-        type.appendChild(type_text);
-        dummy.appendChild(type);
-        var website = document.createElement("p");
-        var website_text = document.createTextNode(arr[i].Website);
-        website.appendChild(website_text);
-        dummy.appendChild(website);
-        var date = document.createElement("p");
-        var date_text = document.createTextNode(arr[i].Date);
-        date.appendChild(date_text);
-        dummy.appendChild(date);
-        var time = document.createElement("p");
-        var time_text = document.createTextNode(arr[i].Time);
-        time.appendChild(time_text);
-        dummy.appendChild(time);
-        var teckitPrice = document.createElement("p");
-        var teckitPrice_text = document.createTextNode(arr[i].TeckitPrice);
-        teckitPrice.appendChild(teckitPrice_text);
-        dummy.appendChild(teckitPrice);
+        var Adress = document.createElement("p");
+        var Adress_text = document.createTextNode(arr[i].Adress);
+        Adress.appendChild(Adress_text);
+        dummy.appendChild(Adress);
+        var ZipCode = document.createElement("p");
+        var ZipCode_text = document.createTextNode(arr[i].ZipCode);
+        ZipCode.appendChild(ZipCode_text);
+        dummy.appendChild(ZipCode);
+        var Image = document.createElement("p");
+        var Image_text = document.createTextNode(arr[i].Image);
+        Image.appendChild(Image_text);
+        dummy.appendChild(Image);
     }
-    ;
 }
-;
-var place_data = data.filter(function (x) { return x.sort == "Place"; });
-var restaurant_data = data.filter(function (x) { return x.sort == "Restaurant"; });
-var event_data = data.filter(function (x) { return x.sort == "Event"; });
-createTabs(place_data, 0);
-createTabs(restaurant_data, 1);
-createTabs(event_data, 2);
+var place_data = locaion_data.filter(function (x) { return x.type == "place"; });
+var reataurant_data = locaion_data.filter(function (x) { return x.type == "reataurant"; });
+var events_data = locaion_data.filter(function (x) { return x.type == "events"; });
+createTabs(reataurant_data, 0);
+createTabs(place_data, 1);
+createTabs(events_data, 2);
--- script.ts
+++ script.ts
@@ -1,116 +1,87 @@
-class location {
-	constructor( public Name , public ZipCode ,public Address, public Image){
+class locaion {
+	constructor(public Name, public ZipCode, public Adress, public Image, public Telephone){
+	}
 
-	};
-};
-
-
-class Place extends location{
-	constructor(Name, ZipCode, Address, Image,sort){
-		this.sort = "Place";
-	};	
-};
+}
 
 
+class place extends locaion {
 
-class Restaurant extends location {
-	constructor(Name, ZipCode, Address, Image, Telephone, Type, Website, sort){
-		super(Name, ZipCode, Address, Image);
-		this.sort = "Restaurant"
-		this.Telephone = "Telephone" ;
-		this.Type = "Type" ;
-		this.Website = "Website" ;
-
-
-	};
-};
+	constructor(Name, ZipCode, Adress, Image, type){
+		super(Name, ZipCode, Adress, Image);
+		this.type = "place";
+	}
+}	
 
 
 
-class Event extends location {
-	constructor(Name, ZipCode, Address, Image, Date, Time, TeckitPrice, sort ){
-		super(Name, ZipCode, Address, Image);
-		this.sort ="Event"
-		this.Date = "Date";
-		this.Time = "Time";
-		this.TeckitPrice = "Teckit Price";
-	};
-};
+class events extends locaion {
+
+	constructor(Name, ZipCode, Adress, Image, type){
+		super(Name, ZipCode, Adress, Image);
+		this.type ="events";
+	}
+}
 
 
-let place1 = new Place("St. Charles Church", "Vienna 1010", "Karlsplatz 1", "src = img/Karlskirche.jpg" );
-let place2 = new Place("Vienna Zoo", "Vienna 1130", "Maxingstraße 13b", "src = img/viennazoo.jpg");
-let restaurant1 = new Restaurant ("Lemon Leaf", "Vienna 1050" ,"Kettenbrückengasse 19", "image", "+43(1)5812308", "Thai Restaurant" , "www.lemonleaf.at");
-let restaurant2 = new Restaurant("SIXTA", "Vienna 1050", "Schönbrunner Straße 21", "image" "+43 1 58 528 56 | +43 1 58 528 56" , "wienerisch", "www.sixta-restaurant.at" );
-let event1 = new Event("Kris Kristofferson" ,"Vienna 1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1" , "image", "Fr., 15. Jun." , "20:00", "58,50 EUR");
-let event2 = new Event("Lenny Kravitz", "Vienna1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "image", "Sat, 09.06.2018", "19:30", "€ 47,80");
-let data= []
-    data.push(place1, place2, restaurant1, restaurant2, event1, event2);
+class reataurant extends locaion {
 
+	constructor(Name, ZipCode, Adress, Image, type){
+		super(Name, ZipCode, Adress, Image);
+		this.type ="reataurant";
+	}	
+}
+
+let place1 = new place("St. Charles Church", "Vienna 1010", "Karlsplatz 1", "Image")
+let place2 = new place("Vienna Zoo", "Vienna 1130", "Maxingstraße 13b", "Image")
+let reataurant1 = new reataurant("Lemon Leaf", "Vienna 1050" ,"Kettenbrückengasse 19", "image", "+43(1)5812308", "Thai Restaurant" , "www.lemonleaf.at")
+let reataurant2 = new reataurant("SIXTA", "Vienna 1050", "Schönbrunner Straße 21", "image", "+43 1 58 528 56 | +43 1 58 528 56" , "wienerisch", "www.sixta-restaurant.at")
+let events1 = new events("Kris Kristofferson" ,"Vienna 1150", "Wiener Stadthalle, Halle F, Roland Rainer Platz 1" , "image", "Fr., 15. Jun." , "20:00", "58,50 EUR")
+let events2 = new events("Lenny Kravitz", "Vienna1150", "Wiener Stadthalle - Halle D, Roland Rainer Platz 1", "image", "Sat, 09.06.2018", "19:30", "€ 47,80")
+
+let locaion_data= []
+	locaion_data.push(place1, place2, reataurant1, reataurant2, events1, events2);
 
 
 function createDOM (){
-	for (i=0;i<data.length;i++){
+	for (i=0;i<locaion_data.length;i++){
  	dummy = document.createElement("div");
  	dummy.classList.add("card");
- 	dummy.classList.add("col-lg-3");
+ 	dummy.classList.add("col-lg-4");
  	dummy.classList.add("col-md-6");
  	dummy.classList.add("col-sm-12");
  	var row = document.getElementById("row4");
  	row.appendChild(dummy);
  	var headline = document.createElement("h4");
- 	var headline_text = document.createTextNode(data[i].Name);
+ 	var headline_text = document.createTextNode(locaion_data[i].Name);
  	headline.appendChild(headline_text);
  	dummy.appendChild(headline);
- 	var name = document.createElement("p");
- 	var name_text = document.createTextNode(data[i].ZipCode);
- 	name.appendChild(name_text);
- 	dummy.appendChild(name);
- 	var address = document.createElement("p");
- 	var address_text = document.createTextNode(data[i].Address);
- 	address.appendChild(address_text);
- 	dummy.appendChild(address);
- 	var zipCode = document.createElement("p");
- 	var zipCode_text = document.createTextNode(data[i].ZipCode);
- 	zipCode.appendChild(zipCode_text);
- 	dummy.appendChild(zipCode);
- 	var image = document.createElement("p");
- 	var image_text = document.createTextNode(data[i].Image);
- 	image.appendChild(image_text);
- 	dummy.appendChild(image);
- 	var telephone = document.createElement("p");
- 	var telephone_text = document.createTextNode(data[i].Telephone);
- 	telephone.appendChild(telephone_text);
- 	dummy.appendChild(telephone);
- 	var type = document.createElement("p");
- 	var type_text = document.createTextNode(data[i].Type);
- 	type.appendChild(type_text);
- 	dummy.appendChild(type);
- 	var website = document.createElement("p");
- 	var website_text = document.createTextNode(data[i].Website);
- 	website.appendChild(website_text);
- 	dummy.appendChild(website);
- 	var date = document.createElement("p");
- 	var date_text = document.createTextNode(data[i].Date);
- 	date.appendChild(date_text);
- 	dummy.appendChild(date);
- 	var time = document.createElement("p");
- 	var time_text = document.createTextNode(data[i].Time);
- 	time.appendChild(time_text);
- 	dummy.appendChild(time);
- 	var teckitPrice = document.createElement("p");
- 	var teckitPrice_text = document.createTextNode(data[i].TeckitPrice);
- 	teckitPrice.appendChild(teckitPrice_text);
- 	dummy.appendChild(teckitPrice);
-	};
-};
+ 	var Adress = document.createElement("p");
+ 	var Adress_text = document.createTextNode(locaion_data[i].Adress);
+ 	Adress.appendChild(Adress_text);
+ 	dummy.appendChild(Adress);
+ 	var ZipCode = document.createElement("p");
+ 	var ZipCode_text = document.createTextNode(locaion_data[i].ZipCode);
+ 	ZipCode.appendChild(ZipCode_text);
+ 	dummy.appendChild(ZipCode);
+ 	var Image = document.createElement("p");
+ 	var Image_text = document.createTextNode(locaion_data[i].Image);
+ 	Image.appendChild(Image_text);
+ 	dummy.appendChild(Image);
+
+	}
+}
+
+
+
+// createDOM();
 
 
 function createTabs (arr, x){
 	for (i=0;i<arr.length;i++){
 	dummy = document.createElement("div");
  	dummy.classList.add("card");
- 	dummy.classList.add("col-lg-3");
+ 	dummy.classList.add("col-lg-4");
  	dummy.classList.add("col-md-6");
  	dummy.classList.add("col-sm-12");
  	var row = document.getElementById(`row${x}`);
@@ -119,50 +90,29 @@ function createTabs (arr, x){
  	var headline_text = document.createTextNode(arr[i].Name);
  	headline.appendChild(headline_text);
  	dummy.appendChild(headline);
- 	var address = document.createElement("p");
- 	var address_text = document.createTextNode(arr[i].Address);
- 	address.appendChild(address_text);
- 	dummy.appendChild(address);
- 	var zipCode = document.createElement("p");
- 	var zipCode_text = document.createTextNode(arr[i].ZipCode);
- 	zipCode.appendChild(zipCode_text);
- 	dummy.appendChild(zipCode);
- 	var image = document.createElement("p");
- 	var image_text = document.createTextNode(arr[i].Image);
- 	image.appendChild(image_text);
- 	dummy.appendChild(image);
- 	var telephone = document.createElement("p");
- 	var telephone_text = document.createTextNode(arr[i].Telephone);
- 	telephone.appendChild(telephone_text);
- 	dummy.appendChild(telephone);
- 	var type = document.createElement("p");
- 	var type_text = document.createTextNode(arr[i].Type);
- 	type.appendChild(type_text);
- 	dummy.appendChild(type);
- 	var website = document.createElement("p");
- 	var website_text = document.createTextNode(arr[i].Website);
- 	website.appendChild(website_text);
- 	dummy.appendChild(website);
- 	var date = document.createElement("p");
- 	var date_text = document.createTextNode(arr[i].Date);
- 	date.appendChild(date_text);
- 	dummy.appendChild(date);
- 	var time = document.createElement("p");
- 	var time_text = document.createTextNode(arr[i].Time);
- 	time.appendChild(time_text);
- 	dummy.appendChild(time);
- 	var teckitPrice = document.createElement("p");
- 	var teckitPrice_text = document.createTextNode(arr[i].TeckitPrice);
- 	teckitPrice.appendChild(teckitPrice_text);
- 	dummy.appendChild(teckitPrice);
+ 	var Adress = document.createElement("p");
+ 	var Adress_text = document.createTextNode(arr[i].Adress);
+ 	Adress.appendChild(Adress_text);
+ 	dummy.appendChild(Adress);
+ 	var ZipCode = document.createElement("p");
+ 	var ZipCode_text = document.createTextNode(arr[i].ZipCode);
+ 	ZipCode.appendChild(ZipCode_text);
+ 	dummy.appendChild(ZipCode);
+ 	var Image = document.createElement("p");
+ 	var Image_text = document.createTextNode(arr[i].Image);
+ 	Image.appendChild(Image_text);
+ 	dummy.appendChild(Image);
+ 		
 
-	};
-};
+	}
+}
 
-var place_data = data.filter(x=>x.sort=="Place");
-var restaurant_data = data.filter(x=>x.sort=="Restaurant");
-var event_data = data.filter(x=>x.sort=="Event");
+var place_data = locaion_data.filter(x=>x.type=="place")
+var reataurant_data = locaion_data.filter(x=>x.type=="reataurant")
+var events_data = locaion_data.filter(x=>x.type=="events")
 
-createTabs(place_data, 0); 
-createTabs(restaurant_data, 1);
-createTabs(event_data, 2);
+createTabs(reataurant_data, 0); 
+createTabs(place_data, 1);
+createTabs(events_data, 2);
+
+
